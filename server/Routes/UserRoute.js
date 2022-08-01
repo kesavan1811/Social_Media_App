39586ef54757548b8f9cf67e955080ceaf44a0c1
Kesavan Ramu <kesavande@gmail.com>
2022-08-01 12:38:34 +0530

    first commit

---
+++ server/Routes/UserRoute.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+  deleteUser,
+  followUser,
+  getAllUsers,
+  getUser,
+  UnFollowUser,
+  updateUser
+} from "../controllers/UserController.js";
+import authMiddleware from "../Middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.get("/", getAllUsers);
+router.get("/:id", getUser);
+router.put("/:id", authMiddleware, updateUser);
+router.delete("/:id", authMiddleware, deleteUser);
+router.put("/:id/follow", authMiddleware, followUser);
+router.put("/:id/unfollow", authMiddleware, UnFollowUser);
+
+export default router;
+   
